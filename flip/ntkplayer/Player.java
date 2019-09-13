e782084e957d25c848e3446b8e95352d24e8c19d
Kanishk Vashisht <kanishk.vashisht@gmail.com>
2019-09-13 18:11:23 -0400

    Kv player wall (#2)
    * pub
    
    * pub
    
    * Matches Wall

--- flip/ntkplayer/Player.java
+++ flip/ntkplayer/Player.java
@@ -11,155 +11,136 @@ import flip.sim.Point;
 import flip.sim.Board;
 import flip.sim.Log;
 
-public class Player implements flip.sim.Player
-{
-	private int seed = 42;
-	private Random random;
-	private boolean isplayer1;
-	private Integer n;
-	private Double diameter_piece;
-	private ArrayList<Integer> offensePieces = new ArrayList<Integer>();
-	private ArrayList<Integer> defensePieces = new ArrayList<Integer>();
+public class Player implements flip.sim.Player {
 
+  // Constants
+  private int seed = 42;
+  private int wallSize = 14;
+  private Random random;
 
-	public Player()
-	{
+  // Init Parameters
+  private boolean isPlayerOne;
+  private Integer numPieces;
+  private Double pieceDiameter;
+
+  // ArrayList
+	private ArrayList<Point> wallCoordinates = new ArrayList<Point>();
+  private HashMap<Point, Integer> coinWallMatches = new HashMap<>();
+
+  // Constructor
+  public Player() {
 		random = new Random(seed);
 	}
 
-	// Initialization function.
-    // pieces: Location of the pieces for the player.
-    // n: Number of pieces available.
-    // t: Total turns available.
-	public void init(HashMap<Integer, Point> pieces, int n, double t, boolean isplayer1, double diameter_piece)
-	{
-		this.n = n;
-		this.isplayer1 = isplayer1;
-		this.diameter_piece = diameter_piece;
+  // Init Function
+  public void init(HashMap<Integer, Point> pieces, int numPieces, double t, boolean isPlayerOne, double pieceDiameter) {
 
-		for(int pieceId : pieces.keySet() ) {
-			if( pieceId < n / 2) {
-				try{
-					this.offensePieces.add(pieceId);
-				}
-				catch(Exception e){
-					System.out.println(e);
-				}
-			}
-			else {
-				try{
-					this.defensePieces.add(pieceId);
-				}
-				catch(Exception e){
-					System.out.println(e);
-				}
+		System.out.println("====");
+		System.out.println("Initializing NTK Player");
+    this.numPieces = numPieces;
+    this.isPlayerOne = isPlayerOne;
+    this.pieceDiameter = pieceDiameter;
 
-			}
-		}
-	}
+    setUpWallCoordinates();
+		System.out.println("Wall Cooodinates Setup");
+    matchCoins(pieces);
+		System.out.println("Initialized NTK Player");
+		System.out.println("====");
 
-	public List<Pair<Integer, Point>> getMoves(Integer num_moves, HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces, boolean isplayer1)
-	{
+  }
 
-		if( (new Random()).nextInt(100) > 50 ) {
-			// RETURN OFFENSIVE STRATEGY
-			return getOffensiveMoves(num_moves, player_pieces, opponent_pieces, isplayer1);
-		}
-		else {
-			// TODO: RETURN DEFENSIVE STRATEGY
-			return getOffensiveMoves(num_moves, player_pieces, opponent_pieces, isplayer1);
-		}
-	}
+  // getMoves
+  public List<Pair<Integer, Point>> getMoves(Integer numMoves, HashMap<Integer, Point> playerPieces, HashMap<Integer, Point> opponent_pieces, boolean isplayer1) {
+    double nearestDistance = Double.POSITIVE_INFINITY;
+    Point chosenWallCoordinate = wallCoordinates.get(0);
 
-	public List<Pair<Integer, Point>> getOffensiveMoves(Integer num_moves, HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces, boolean isplayer1)
-	{
-		 List<Pair<Integer, Point>> moves = new ArrayList<Pair<Integer, Point>>();
+    for(Point wallCord : wallCoordinates){
+        Integer matchedCoinId = coinWallMatches.get(wallCord);
+        Point matchedCoinPoint = playerPieces.get(matchedCoinId);
+        double distance = calculateEuclideanDistance(wallCord.x, wallCord.y, matchedCoinPoint.x, matchedCoinPoint.y);
 
-		 int num_trials = 30;
-		 int i = 0;
-
-		 while(moves.size()!= num_moves && i<num_trials)
-		 {
-
-			// Picking a random offensive piece
-		 	int piece_idx = random.nextInt(offensePieces.size());
-			Integer piece_id = offensePieces.get(piece_idx);
-
-		 	Point curr_position = player_pieces.get(piece_id);
-		 	Point new_position = new Point(curr_position);
-
-		 	double theta = -Math.PI/2 + Math.PI * random.nextDouble();
-		 	double delta_x = diameter_piece * Math.cos(theta);
-		 	double delta_y = diameter_piece * Math.sin(theta);
-
-		 	Double val = (Math.pow(delta_x,2) + Math.pow(delta_y, 2));
-		 	// System.out.println("delta_x^2 + delta_y^2 = " + val.toString() + " theta values are " +  Math.cos(theta) + " " +  Math.sin(theta) + " diameter is " + diameter_piece);
-		 	// Log.record("delta_x^2 + delta_y^2 = " + val.toString() + " theta values are " +  Math.cos(theta) + " " +  Math.sin(theta) + " diameter is " + diameter_piece);
-
-		 	new_position.x = isplayer1 ? new_position.x - delta_x : new_position.x + delta_x;
-		 	new_position.y += delta_y;
-		 	Pair<Integer, Point> move = new Pair<Integer, Point>(piece_id, new_position);
-
-		 	Double dist = Board.getdist(player_pieces.get(move.getKey()), move.getValue());
-		 	// System.out.println("distance from previous position is " + dist.toString());
-		 	// Log.record("distance from previous position is " + dist.toString());
-
-		 	if(check_validity(move, player_pieces, opponent_pieces)){
-		 		moves.add(move);
-		 		System.out.print("offense size ");
-		 		System.out.println(offensePieces.size());
-		 		if((isplayer1 && new_position.x < -25) || (!isplayer1 && new_position.x > 25)){
-		 			offensePieces.remove(piece_idx);
-		 			System.out.print("piece_idx ");
-		 			System.out.println(piece_idx);
-		 			int d_piece_idx = random.nextInt(defensePieces.size());
-					Integer d_piece_id = defensePieces.get(d_piece_idx);
-					offensePieces.add(d_piece_id);
-		 		}
-		 	}
-		 	i++;
-		 }
-
-		 return moves;
-	}
-
-	// return the next piece to move to build a Wall
-	public int getNextWallPiece(HashMap<Integer, Point> player_pieces){
-		//arraylist of points as wall coordinates
-		Point pos = wall.get(0);
-		double minDist = Double.POSITIVE_INFINITY;
-		double minPiece = -1;
-		for(Integer id : wallPieces){
-			Point piece = player_pieces.get(id);
-			double dist = calculateDistance(pos.x, pos.y, piece.x, piece.y);
-			if(dist < minDist){
-				minDist = dist;
-				minPiece = id;
-			}
-		}
-		return minPiece;
-	}
-
-	public double calculateDistance(double x1, double y1, double x2, double y2){
-		return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
-	}
-
-	public boolean check_validity(Pair<Integer, Point> move, HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces)
-    {
-        boolean valid = true;
-
-        // check if move is adjacent to previous position.
-        if(!Board.almostEqual(Board.getdist(player_pieces.get(move.getKey()), move.getValue()), diameter_piece))
-            {
-                return false;
-            }
-        // check for collisions
-        valid = valid && !Board.check_collision(player_pieces, move);
-        valid = valid && !Board.check_collision(opponent_pieces, move);
-
-        // check within bounds
-        valid = valid && Board.check_within_bounds(move);
-        return valid;
-
+        if(distance < nearestDistance) {
+          nearestDistance = distance;
+          chosenWallCoordinate = wallCord;
+        }
     }
+
+    Integer nearestCoinId = coinWallMatches.get(chosenWallCoordinate);
+    Point nearestCoinPoint = playerPieces.get(nearestCoinId);
+    double deltaY = chosenWallCoordinate.y - nearestCoinPoint.y;
+    double deltaX = chosenWallCoordinate.x - nearestCoinPoint.x;
+    double theta = Math.atan(deltaY/deltaX);
+
+    List<Pair<Integer, Point>> moves = new ArrayList<Pair<Integer, Point>>();
+
+    // first move
+    double newX = nearestCoinPoint.x + (2*Math.cos(theta));
+    double newY = nearestCoinPoint.y + (2*Math.sin(theta));
+		System.out.println(newX);
+		System.out.println(newY);
+    Point newPosition = new Point(newX, newY);
+    moves.add(new Pair(nearestCoinId, newPosition));
+
+    // second move
+    newX +=  (2/Math.cos(theta));
+    newY +=  (2/Math.sin(theta));
+    newPosition = new Point(newX, newY);
+    moves.add(new Pair(nearestCoinId, newPosition));
+
+    return moves;
+  }
+
+
+  // Our Functions
+
+  public void setUpWallCoordinates(){
+    int wallSize = this.wallSize;
+    double height = 40;
+    double y_top = height/2;
+    double diameter = this.pieceDiameter;
+    double totalOccupiedSpace = diameter*wallSize;
+    double totalGap = height - totalOccupiedSpace;
+    double gap = totalGap/(wallSize+1);
+    double[] yCoordinates = new double[wallSize];
+
+    // based on what side of board we're on
+    double xCoordinate = this.isPlayerOne ? -19 : 19;
+
+    yCoordinates[0] = y_top - gap - diameter/2;
+    for(int i = 1; i < wallSize; i++){
+      yCoordinates[i] = yCoordinates[i-1]-diameter-gap;
+    }
+
+    for(double yCoordinate: yCoordinates){
+      double roundedY = Math.round(yCoordinate * 100.0) / 100.0;
+      wallCoordinates.add(new Point(xCoordinate, roundedY));
+    }
+  }
+
+  public void matchCoins(HashMap<Integer, Point> coins){
+		List<Integer> coinKeys = new ArrayList<Integer>(coins.keySet());
+    for(Point wallCord : wallCoordinates){
+      double minDist = Double.POSITIVE_INFINITY;
+      int minCoin = -1;
+      for(Integer coinId: coinKeys){
+        Point coin = coins.get(coinId);
+        double dist = calculateEuclideanDistance(coin.x, coin.y, wallCord.x, wallCord.y);
+        if(dist < minDist){
+          minDist = dist;
+          minCoin = coinId;
+        }
+      }
+			try{
+      coinKeys.remove(new Integer(minCoin));
+      coinWallMatches.put(wallCord, minCoin);
+		} catch(Exception e){
+			System.out.println(e);
+		}
+    }
+		return;
+	}
+
+  public double calculateEuclideanDistance(double x1, double y1, double x2, double y2){
+    return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+  }
 }
