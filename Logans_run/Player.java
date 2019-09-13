3492e7aa9970034f1ddc0dc8490ba3d9c33cd324
Kanishk Vashisht <kanishk.vashisht@gmail.com>
2019-09-13 00:03:56 -0400

    updating based on team submissions (#1)
    * Created player code files for group 6
    
    * Set up code for different strategies
    
    * Add density player
    
    Move straight the coin, whose stripe has the lower density and create a
    barrier in the oponent side. In case this strategy doesn't know how to
    move, just behave like random.
    
    Co-authored-by: Ana Maria Martinez Gomez <anamaria@martinezgomez.name>
    Co-authored-by: Adam Svystun <adam.svystun@gmail.com>
    Co-authored-by: Kelley Paul Valentine <kpv2106@columbia.edu>
    
    * initial deliverable for Wednesday 9/11
    
    * Made small updates to setup code
    
    * Added a couple of methods to Move abstract class
    
    * Obstacle Creation
    
    * Obstacle Creation and Agression - Shirish
    
    * Added code to make half coins do aggressive, half CD
    
    * For demo, adding Shirish's code for Player
    
    * Add files via upload
    
    * Delete Player.java
    
    * Shirish Code, Vanilla
    
    * Add files via upload

---
+++ Logans_run/Player.java
@@ -0,0 +1,109 @@
+package flip.players;
+import java.util.List;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.HashMap;
+import javafx.util.Pair; 
+import java.util.ArrayList;
+
+import flip.sim.Point;
+import flip.sim.Board;
+import flip.sim.Log;
+
+public class Player implements flip.sim.Player
+{
+	private int seed = 42;
+	private Random random;
+	private boolean isplayer1;
+	private Integer n;
+	private Double diameter_piece;
+
+
+	public Player()
+	{
+		random = new Random(seed);
+	}
+
+	// Initialization function.
+    // pieces: Location of the pieces for the player.
+    // n: Number of pieces available.
+    // t: Total turns available.
+	public void init(HashMap<Integer, Point> pieces, int n, double t, boolean isplayer1, double diameter_piece)
+	{
+		this.n = n;
+		this.isplayer1 = isplayer1;
+		this.diameter_piece = diameter_piece;
+	}
+
+	public boolean inEndZone(double x, boolean isplayer1) {
+		if((!isplayer1 && x > 20 + 1.75 * diameter_piece + ((n / 9) * (diameter_piece / 2))) || (isplayer1 && x < -20 - 1.75 * diameter_piece - ((n / 9) * (diameter_piece / 2)))) {
+			return true;
+		}
+		return false;
+	}
+
+	public List<Pair<Integer, Point>> getMoves(Integer num_moves, HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces, boolean isplayer1)
+	{
+		 List<Pair<Integer, Point>> moves = new ArrayList<Pair<Integer, Point>>();
+
+		 int num_trials = 30;
+		 int i = 0;
+
+		 while(moves.size()!= num_moves && i<num_trials)
+		 {
+		 	Integer piece_id = random.nextInt(n);
+		 	Point curr_position = player_pieces.get(piece_id);
+		 	if(inEndZone(curr_position.x, isplayer1)) {
+		 		i++;
+		 		continue;
+		 	}
+		 	Point new_position = new Point(curr_position);
+
+		 	double theta = -Math.PI/2 + Math.PI * random.nextDouble();
+		 	if((isplayer1 && curr_position.x > 20 )|| (!isplayer1 && curr_position.x < -20)) {
+		 		theta = 0;
+		 	}
+		 	double delta_x = diameter_piece * Math.cos(theta);
+		 	double delta_y = diameter_piece * Math.sin(theta);
+
+		 	Double val = (Math.pow(delta_x,2) + Math.pow(delta_y, 2));
+		 	// System.out.println("delta_x^2 + delta_y^2 = " + val.toString() + " theta values are " +  Math.cos(theta) + " " +  Math.sin(theta) + " diameter is " + diameter_piece);
+		 	// Log.record("delta_x^2 + delta_y^2 = " + val.toString() + " theta values are " +  Math.cos(theta) + " " +  Math.sin(theta) + " diameter is " + diameter_piece);
+
+		 	new_position.x = isplayer1 ? new_position.x - delta_x : new_position.x + delta_x;
+		 	new_position.y += delta_y;
+		 	Pair<Integer, Point> move = new Pair<Integer, Point>(piece_id, new_position);
+
+		 	Double dist = Board.getdist(player_pieces.get(move.getKey()), move.getValue());
+		 	// System.out.println("distance from previous position is " + dist.toString());
+		 	// Log.record("distance from previous position is " + dist.toString());
+
+		 	if(check_validity(move, player_pieces, opponent_pieces))
+		 		moves.add(move);
+		 	i++;
+		 }
+		 
+		 return moves;
+	}
+
+	public boolean check_validity(Pair<Integer, Point> move, HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces)
+    {
+        boolean valid = true;
+       
+        // check if move is adjacent to previous position.
+        if(!Board.almostEqual(Board.getdist(player_pieces.get(move.getKey()), move.getValue()), diameter_piece))
+            {
+                return false;
+            }
+        // check for collisions
+        valid = valid && !Board.check_collision(player_pieces, move);
+        valid = valid && !Board.check_collision(opponent_pieces, move);
+
+        // check within bounds
+        valid = valid && Board.check_within_bounds(move);
+        return valid;
+
+    }
+}
+
