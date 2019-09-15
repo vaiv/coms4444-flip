1c4b6a89fae562b86e490944675a8002e482c495
Adam Svystun <adam.svystun@gmail.com>
2019-09-15 18:52:36 -0400

    Improve density calculation (#1)
    * Update density calculation
    
    * Fix typing
    
    * Update numbers
    
    * Fix signs in density cones

--- flip/density/Player.java
+++ flip/density/Player.java
@@ -18,7 +18,9 @@ public class Player implements flip.sim.Player
 	private boolean isplayer1;
 	private Integer n;
 	private Double diameter_piece;
-	private Double distance = 5.0;
+	private Double distance = 3.73; // ~= 2 + sqrt(3)
+	private Double density_cone_height = 4.0;
+	private Double density_lane_gap = 1.0;
 	private Integer threadfold = 30;
 
 
@@ -50,8 +52,18 @@ public class Player implements flip.sim.Player
 				|| (!isplayer1 && curr_position.x > threadfold)) || !check_validity(move, player_pieces, opponent_pieces)) continue;
 			int count = 0;
 			for (Point point : opponent_pieces.values()) {
+				// Two lines parameters
+				double k1 = -sign * distance / density_cone_height;
+				double b1 = curr_position.y  - k1 * (curr_position.x - sign * density_lane_gap);
+				double k2 = sign * distance / density_cone_height;
+				double b2 = curr_position.y  - k2 * (curr_position.x - sign * density_lane_gap);
+				double x = point.x;
 				double y = point.y;
-				if (y > curr_position.y - distance && y < curr_position.y + distance) 
+				boolean condition1 = y < (curr_position.y + distance);
+				boolean condition2 = y > (curr_position.y - distance);
+				boolean condition3 = (x * k1 + b1 - y) < 0;
+				boolean condition4 = (x * k2 + b2 - y) > 0;
+				if(condition1 && condition2 && condition3 && condition4)
 					count++;
 			}
 			if (low1 > count) {
@@ -70,8 +82,9 @@ public class Player implements flip.sim.Player
 			Pair<Integer, Point> move1 = new Pair<Integer, Point>(low1Id, new Point(point1.x + sign*2, point1.y));
 			moves.add(move1);
 			Pair<Integer, Point> move2 = new Pair<Integer, Point>(low1Id, new Point(point1.x + sign*4, point1.y));
-			if(check_validity(move2, player_pieces, opponent_pieces)) 
+			if(check_validity(move2, player_pieces, opponent_pieces)) {
 				moves.add(move2);
+			}
 			else if (low2Id != -1){
 				Point point2 = player_pieces.get(low2Id);
 				Pair<Integer, Point> move3 = new Pair<Integer, Point>(low2Id, new Point(point2.x + sign*2, point2.y));
@@ -82,10 +95,8 @@ public class Player implements flip.sim.Player
 		int num_trials = 30;
 		int i = 0;
 
-		 while(moves.size()!= num_moves && i<num_trials)
-		 {
-			 
-		 	Integer piece_id = random.nextInt(n);
+		while(moves.size()!= num_moves && i<num_trials)  {
+			Integer piece_id = random.nextInt(n);
 			
 		 	Point curr_position = player_pieces.get(piece_id);
 			if(((isplayer1 && curr_position.x < -threadfold)
