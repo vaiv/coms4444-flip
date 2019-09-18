2fa61512024d5b982c7645151e03c0cc94645c8d
Derek Ren <guanguangua@gmail.com>
2019-09-18 05:26:57 -0400

    fixed wall

--- flip/g8wall/Player.java
+++ flip/g8wall/Player.java
@@ -23,12 +23,16 @@ public class Player implements flip.sim.Player
 	private Integer n;
 	private Double diameter_piece;
 
-	// Stage variabl: if the wall has been initialized
+	// Stage variable: if the wall has been initialized
 	private boolean wall_init = false;
+	// Stage variable: if the wall has finished
+	private boolean wall_complete = false;
 	// points that's used to build a wall
 	private HashMap<Integer, Point> wall_point = new HashMap<Integer, Point>();
 	// indicates if a coin should be not be moved, like coins forming a wall
 	private HashSet<Integer> ignored_piece = new HashSet<Integer>();
+	// indexes for attackers
+	private HashSet<Integer> attackers = new HashSet<Integer>();
 	// temporary variable to store current destination of each piece
 	private ArrayList<Pair<Integer, Point>> piece_to_dest = new ArrayList<Pair<Integer, Point>>();
 	// cached moves, the moves is always valid
@@ -57,9 +61,11 @@ public class Player implements flip.sim.Player
 		int num_trials = 30;
 		int i = 0;
 
+		// TODO: if n < 20, use greedy
+
 		// Set current wall buidling objective
 		if (!wall_init) {
-			ArrayList<Point> wall = wall_points(isplayer1 ? 16.0 : -16.0, 1.35); // positions for wall
+			ArrayList<Point> wall = find_wall_points(isplayer1 ? 21 : -21, 1.45, 1.75); // positions for wall
 			for (Point point : wall) {
 				Integer id = closest(point, player_pieces, wall_point);
 				piece_to_dest.add(new Pair<Integer, Point>(id, point));
@@ -74,7 +80,7 @@ public class Player implements flip.sim.Player
 		// 	Point dest = pair.getValue();
 		// 	if (ignored_piece.contains(id)) continue;
 		// 	while (moves.size() < num_moves && i < num_trials) {
-		// 		Point temp = find_move(player_pieces.get(id), dest);
+		// 		Point temp = find_move_exact(player_pieces.get(id), dest);
 		// 		Pair<Integer, Point> move = new Pair<Integer, Point>(id, temp);
 		// 		if(check_validity(move, player_pieces, opponent_pieces))
 		// 			moves.add(move);
@@ -82,19 +88,76 @@ public class Player implements flip.sim.Player
 		// 	}
 		// }
 
-		// TODO: mark coins in the wall position
 		for (Pair<Integer, Point> pair : piece_to_dest) {
+			if (wall_complete) break;
 			Integer id = pair.getKey();
 			Point dest = pair.getValue(); 
+
+			// mark the coins already in wall position, skip those already in position
+			if (ignored_piece.contains(id)) continue;
+			if (Board.almostEqual(distance(player_pieces.get(id), dest), 0.0)) {
+				ignored_piece.add(id);
+				continue;
+			}
+
 			if (moves.size() >= num_moves || i >= num_trials) 
 				break;
-			Point temp = find_move_exact(player_pieces.get(id), dest);
+			
+			Point temp = find_move_exact(player_pieces.get(id), dest, 1); // find the next move
 			Pair<Integer, Point> move = new Pair<Integer, Point>(id, temp);
 			if(check_validity(move, player_pieces, opponent_pieces))
 				moves.add(move);
+			else { // Try move the coin in other direction
+				temp = find_move_exact(player_pieces.get(id), dest, -1);
+				move = new Pair<Integer, Point>(id, temp);
+				if (check_validity(move, player_pieces, opponent_pieces)) 
+					moves.add(move);
+				else { // move backwards to make space
+					// TODO: random
+					temp = player_pieces.get(id);
+					temp.x -= diameter_piece;
+					move = new Pair<Integer, Point>(id, temp);
+				}
+			}
 			++i;	 
 		}
 
+		// Wall complete
+		if (ignored_piece.size() == wall_point.size()) wall_complete = true;
+
+		// start attacking
+		// TODO: attack
+		if (wall_complete) {
+			int wall_size = wall_point.size();
+			ArrayList<Integer> wall_point_index = new ArrayList<Integer>(wall_point.keySet());
+			ArrayList<Point> wall_point_array = new ArrayList<Point>(wall_point.values());
+			wall_point_array.sort((Point p1, Point p2) -> (p1.y > p2.y) ? 1 : -1);
+
+			Point attack_pos = new Point(wall_point_array.get(wall_size - 1)); // position to start the attack
+			Integer wall_index = closest(attack_pos, player_pieces, (HashSet<Integer>)null); // wall next to the attacker
+			attack_pos.x -= isplayer1 ? -diameter_piece : diameter_piece;
+			Integer attacker = closest(attack_pos, player_pieces, ignored_piece);
+			while (moves.size() < num_moves && i < num_trials) {
+				Point temp = find_move_exact(player_pieces.get(attacker), attack_pos, 1);
+				Pair<Integer, Point> move = new Pair<Integer, Point>(attacker, temp);
+				if(check_validity(move, player_pieces, opponent_pieces)) {
+					moves.add(move);
+					temp = find_move_exact(temp, attack_pos, 1);
+					move = new Pair<Integer, Point>(attacker, temp);
+					moves.add(move);
+				}
+				else {
+					temp = find_move_exact(player_pieces.get(attacker), attack_pos, -1);
+					move = new Pair<Integer, Point>(attacker, temp);
+					if (check_validity(move, player_pieces, opponent_pieces))
+						moves.add(move);
+					else {
+						// TODO: random
+					}
+				}
+				++i;
+			}
+		}
 		return moves;
 	}
 
@@ -107,10 +170,10 @@ public class Player implements flip.sim.Player
 	}
 
 	// Find a valid move that moves a coin exactly to the destination
+	// when there are two possible moves, set choice to -1 will give different result, choice default to 1 
 	// TODO: more advanced path finding algorithm, avoid obstacles, refractor
-	// WARNING: this one only works when start and dest are both on the left or right side
-	// please only use this for building walls
-	public Point find_move_exact(Point start, Point dest) {
+	// WARNING: this function one only works when start and dest are both on the left or right side
+	public Point find_move_exact(Point start, Point dest, int choice) {
 		if (isplayer1) { // Very questionable logic here
 			start = new Point(start);
 			dest = new Point(dest);
@@ -125,7 +188,7 @@ public class Player implements flip.sim.Player
 		if (Board.almostEqual(dist, length)) {
 			result = new Point(dest);
 		}
-		else if (dist <= 2 * length) { // TODO: 2 * length
+		else if (dist <= 2 * length) { 
 			double newX = start.x;
 			double newY = start.y;
 			double vector1_x, vector1_y, vector2_x, vector2_y;
@@ -134,8 +197,8 @@ public class Player implements flip.sim.Player
 			double vector1_length = dist / 2;
 			double vector2_length = Math.sqrt(Math.pow(length, 2) - Math.pow(vector1_length, 2));
 
-			vector2_x = -(vector1_y / vector1_length) * vector2_length;
-			vector2_y = (vector1_x / vector1_length) * vector2_length;
+			vector2_x = choice*(-(vector1_y / vector1_length) * vector2_length);
+			vector2_y = choice*((vector1_x / vector1_length) * vector2_length);
 
 			newX += (vector1_x + vector2_x);
 			newY += (vector1_y + vector2_y);
@@ -152,6 +215,28 @@ public class Player implements flip.sim.Player
 		}
 		return result;
 	}
+
+	// Find two moves that moves a coin to the destination
+	// public ArrayList<Pair<Integer, Point>> find_two_moves(Point start, Point dest, Integer index, 
+	// 	   HashMap<Integer, Point> player_pieces, HashMap<Integer, Point> opponent_pieces) {
+	// 	ArrayList<Pair<Integer, Point>> moves = new ArrayList<Pair<Integer, Point>>();
+	// 	Point temp = find_move_exact(start, dest, 1);
+	// 	Pair<Integer, Point> move = new Pair<Integer, Point>(index, temp);
+	// 	if(check_validity(move, player_pieces, opponent_pieces)) {
+	// 		moves.add(move);
+	// 		temp = find_move_exact(temp, dest, 1);
+	// 		move = new Pair<Integer, Point>(attacker, temp);
+	// 			}
+	// 			else {
+	// 				temp = find_move_exact(player_pieces.get(attacker), attack_pos, -1);
+	// 				move = new Pair<Integer, Point>(attacker, temp);
+	// 				if (check_validity(move, player_pieces, opponent_pieces))
+	// 					moves.add(move);
+	// 				else {
+	// 					// TODO: random
+	// 				}
+	// 			}
+	// }
 
 	// Find the id of a coin that's closest to a point, ignoring some visited
 	public Integer closest(Point point, HashMap<Integer, Point> pieces, HashMap<Integer, Point> ignore) {
@@ -170,15 +255,32 @@ public class Player implements flip.sim.Player
 		}
 		return result_id;
 	}
+
+	public Integer closest(Point point, HashMap<Integer, Point> pieces, HashSet<Integer> ignore) {
+		double min_distance = Double.MAX_VALUE;
+		Integer result_id = 0;
+		for (int i = 0; i < n; ++i) {
+			if (ignore != null && ignore.contains(i)) 
+				continue;
+			Integer id = i;
+			Point p = pieces.get(id);
+			double distance = Math.sqrt(Math.pow(point.x - p.x, 2) + Math.pow(point.y - p.y, 2));
+			if (distance < min_distance) {
+				min_distance = distance;
+				result_id = id;
+			} 
+		}
+		return result_id;
+	}
 	
 	// Find a list of points at x axis that can form a vertical wall
 	// spacing is the space between each coin
-	public ArrayList<Point> wall_points(double x, double spacing) {
+	public ArrayList<Point> find_wall_points(double x, double spacing, double start) {
 		ArrayList<Point> result = new ArrayList<Point>();
 		double d = this.diameter_piece;
 		double y_max = 20.0;
 		double y_min = -20.0;
-		double curr_y = y_min + d/2;
+		double curr_y = y_min + d/2 + start;
 		for (;curr_y + d/2 <= y_max; curr_y += d + spacing) {
 			result.add(new Point(x, curr_y));
 		}
